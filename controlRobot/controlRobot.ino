f9e4c27bad9fc011cb370de7859ba773b8b3b160
Ísaac <isaacfelipe2007@gmail.com>
2024-04-20 10:24:14 -0600

    Agregué código funcional, con algunos bugs, del contrtol completo del robot

--- controlRobot/controlRobot.ino
+++ controlRobot/controlRobot.ino
@@ -1,7 +1,10 @@
+#include <Servo.h>          // Libreria para servo
+
 #define PUL 7               // Cada pulso a este pin es un paso (o micropaso) del motor
 #define DIR 6               // Define la dirección del motor, HIGH es horario
-#define botonReinicio 2     // Botón para volver el motor a posición inicial (0 grados)
 #define botonBusqueda 3     // Botón para iniciar secuencia de clasificación de barquillos
+#define botonDerecha 4
+#define botonIzquierda 5
 #define sensorHall A0       // Pin de lectura del sensor Hall
 
 const int stepsPorRev = 200;                    // Motor estándar de 200 pasos por revolución
@@ -10,72 +13,107 @@ const int Za = 16;                              // Dientes del engranaje central
 const int Zc = 48;                              // Dientes del engranaje anular
 const float relacionEngranes = Zc/Za + 1;       // Relación de engranajes para sistema planetario con carrier fijo
 const float pasosPorRev = stepsPorRev * microsteps * relacionEngranes;        // Pasos que requiere dar el motor para dar una revolución completa
-const int delayStep = 50;                       // Tiempo en microsegundos que hay entre cada pulso de paso. Entre más pequeño, más rápido
+const int delayStep = 500;                      // Tiempo en microsegundos que hay entre cada pulso de paso. Entre más pequeño, más rápido
 float posActual = 0;                            // Posición actual del motor que se irá actualizando
 const int anguloLimite = 55;                    // Angulo del limite del arco a recorrer
 const int limitePosicion = floor(pasosPorRev/360.0*anguloLimite);                // Pasos que hay desde el 0 hasta el (aproximadamente) angulo limite
 unsigned int cantidadPasos = 0;                 // Cuenta cuantos pasos se ha movido 
+int sensorHallThreshold = 528;                  // Umbral para detectar campo magnético
+bool direccion;                                 // Direccion de giro del stepper
+bool noEncontroIman = 0;
+
+Servo myservo;                                  // Se define el objeto servo
+const int cierre = 55;                          // Valor determinado experimentalmente con el cual la garra puede tomar un barquillo
+const int apertura = 155;                       // Valor determinado experimentalmente con el cual la garra abre lo sudiciente para tomar un barquillo
+bool busqueda = 0;                              // Memoria de botón
 
 void setup() {
   pinMode(PUL, OUTPUT);
   pinMode(DIR, OUTPUT);
-  pinMode(botonReinicio, INPUT);
   pinMode(botonBusqueda, INPUT);
+  pinMode(botonDerecha, INPUT);
+  pinMode(botonIzquierda, INPUT);
+  myservo.attach(9);                            // Salida para control del servo
+  Serial.begin(115200);
 }
 
 void loop() {
-  bool modoReinicio = digitalRead(botonReinicio);
-  bool modoBusqueda = digitalRead(botonBusqueda);
-  if (modoReinicio) {
-    secuenciaReinicio();
+  if(digitalRead(botonBusqueda)) {
+      busqueda = 1;
+      delay(2000);
+      myservo.write(cierre);        // Cerrar garra
+      delay(2000);
+      direccion = 0;                // Empieza girando a la derecha
+      digitalWrite(DIR, direccion);
   }
-  if(modoReinicio) {
-    secuenciaBusqueda();
+  Serial.println(busqueda);
+  if(busqueda) {
+    int medicionHall = analogRead(sensorHall);
+    if(medicionHall < sensorHallThreshold) {
+      moverStepper(1);        // Mover stepper un paso
+      posActual++;
+      if(posActual >= limitePosicion && !noEncontroIman) {
+        direccion = 1;
+        digitalWrite(DIR, direccion);
+        posActual = 0;
+        noEncontroIman = 1;
+      }
+      else if(posActual >= 2*limitePosicion && noEncontroIman) {
+        direccion = 0;
+        digitalWrite(DIR, direccion);
+        moverStepper(limitePosicion);
+        posActual = 0;
+        noEncontroIman = 0;
+        busqueda = 0;
+        delay(2000);
+        myservo.write(apertura);
+
+      }
+    }
+    else {
+      delay(2000);
+      myservo.write(apertura);
+      delay(2000);
+      busqueda = 0;
+      if(direccion) {
+        direccion = 0;
+        digitalWrite(DIR, direccion);
+        if(posActual <= limitePosicion/2) {
+          moverStepper(limitePosicion/2 - posActual);
+        }
+        else {
+          moverStepper(posActual - limitePosicion/2);
+        }
+      }
+      else {
+        direccion = 1;
+        digitalWrite(DIR, direccion);
+        moverStepper(posActual);
+      }
+    }
   }
+  if(digitalRead(botonDerecha)) {
+    direccion = 0;
+    digitalWrite(DIR, direccion);
+    while(digitalRead(botonDerecha)) {
+      moverStepper(1);
+    }
+  }
+  if(digitalRead(botonIzquierda)) {
+    direccion = 1;
+    digitalWrite(DIR, direccion);
+    while(digitalRead(botonIzquierda)) {
+      moverStepper(1);
+    }
+  }
+
 }
 
-void secuenciaBusqueda() {
-  // INSERTAR LECTURA DEL HALL
-
-  digitalWrite(DIR,0);        // Primero gira hacia la derecha
-  for (int paso = 0; paso < limitePosicion; paso++)       // Gira hasta el final
-  {
-    // CONDICIÓN DE SI DETECTA HALL, PARE Y ABRA GARRA
-
-    digitalWrite(PUL,HIGH);
+void moverStepper(int pasos) {
+  for(int paso = 0; paso < pasos; paso++) {
+    digitalWrite(PUL, HIGH);
     delayMicroseconds(delayStep);
-    digitalWrite(PUL,LOW);
-    delayMicroseconds(delayStep);
-    cantidadPasos++;
-  }
-  digitalWrite(DIR,1);        // Primero gira hacia la izquierda
-  for (int paso = 0; paso < 2*limitePosicion; paso++)       // Gira hasta el otro lado (doble recorrido)
-  {
-    // CONDICIÓN DE SI DETECTA HALL, PARE Y ABRA GARRA
-
-    digitalWrite(PUL,HIGH);
-    delayMicroseconds(delayStep);
-    digitalWrite(PUL,LOW);
-    delayMicroseconds(delayStep);
-    cantidadPasos++;
-  }
-
-
-}
-
-void secuenciaReinicio() {
-  if (posActual > 0) {
-    digitalWrite(DIR,1);
-  }
-  else if (posActual < 0) {
-    digitalWrite(DIR,0);
-  }
-  for (int paso = 0; paso < abs(posActual); paso++)    
-  {
-    digitalWrite(PUL,HIGH);
-    delayMicroseconds(delayStep);
-    digitalWrite(PUL,LOW);
+    digitalWrite(PUL, LOW);
     delayMicroseconds(delayStep);
   }
-  posActual = 0;
 }
